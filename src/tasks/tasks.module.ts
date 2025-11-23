106874be25068bf977c4e514063265968d0881ed
Leonardo Paublo <leonardopaublo@gmail.com>
2025-11-23 11:50:46 -0300

    chore(): fixing relative imports

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { Task } from './task.entity';
 import { DataSource } from 'typeorm';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task]), AuthModule],
